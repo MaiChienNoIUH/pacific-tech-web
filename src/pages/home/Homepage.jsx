66d5e4f1a90bef0624c0d6c494f68907c2e801a6
MaiChienNo <nomai6789@gmail.com>
2026-05-04 13:28:18 -0700

    feat: add Partner section with marquee logo animation

--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -6,6 +6,7 @@ import Services from "./components/Services";
 import About from "./components/About";
 import Blog from "./components/Blog";
 import CTA from "./components/CTA";
+import Partner from "./components/Partner";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
+      <Partner />
       <Blog />
       <CTA />
       <Footer />
